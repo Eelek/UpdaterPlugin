491c52b890d23e023bed5345b230c54339404063
Eelek <eelke.spijk@gmail.com>
2019-09-23 21:30:19 +0200

    First Commit

---
+++ src/tk/projectcraftmc/updater/Mapper.java
@@ -0,0 +1,212 @@
+package tk.projectcraftmc.updater;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Mapper {
+	
+	private UpdaterMain plugin;
+	
+	private final int WIDTH = 128;
+	private final int HEIGHT = 128;
+	private HashMap<Material, Integer> colorIndex;
+	private ArrayList<Location> editedChunks;
+
+	public Mapper(UpdaterMain instance) {
+		plugin = instance;
+
+		editedChunks = new ArrayList<Location>();
+
+		try {
+			String colorData = plugin.getDataFromWebserver(plugin.getConfig().getString("map-data-url"));
+
+			colorIndex = new HashMap<Material, Integer>();
+
+			JSONParser parser = new JSONParser();
+			Object object = parser.parse(colorData);
+			JSONArray ids = (JSONArray) object;
+
+			for (int i = 0; i < ids.size(); i++) {
+				JSONArray id = (JSONArray) ids.get(i);
+
+				for (int m = 0; m < id.size(); m++) {
+					Material material = Material.getMaterial(id.get(m).toString().toUpperCase());
+					if (material == null) {
+						throw new IllegalArgumentException(String.valueOf(id.get(m).toString()) + " is not a valid material.");
+					}
+					colorIndex.put(material, Integer.valueOf(i));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public void updateMap() {
+		plugin.getServer().broadcastMessage("Updating map, this may be laggy.");
+
+		ArrayList<Location> current = editedChunks;
+
+		try {
+			JSONArray cache = getChunkCache();
+
+			for (Object location : cache) {
+				String[] loc = location.toString().split(",");
+
+				current.add(new Location(plugin.getServer().getWorld(loc[0]), Integer.parseInt(loc[1]), 0.0D, Integer.parseInt(loc[2])));
+			}
+
+			writeChunkCache(new JSONArray());
+
+			for (Location loc : current) {
+				JSONArray data = new JSONArray();
+				data.add(Arrays.toString(generateMap(loc.getWorld(), loc.getBlockX(), loc.getBlockZ(), false, WIDTH, HEIGHT)));
+				plugin.sendDataToWebserver("data=" + data.toJSONString(), plugin.getConfig().getString("image-api-url"));
+			}
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		}
+
+		plugin.getConfig().set("last-render-update", Long.valueOf(System.currentTimeMillis()));
+	}
+
+	@SuppressWarnings("unchecked")
+	public void saveEditedChunks() {
+		ArrayList<Location> copy = editedChunks;
+
+		try {
+			JSONArray cache = getChunkCache();
+
+			for (Location location : copy) {
+				String loc = location.getWorld().getUID() + "," + location.getBlockX() + "," + location.getBlockZ();
+				if (cache.contains(loc))
+					return;
+				cache.add(loc);
+			}
+
+			writeChunkCache(cache);
+		} catch (ParseException | IOException e) {
+			e.printStackTrace();
+		}
+
+		editedChunks.removeAll(copy);
+
+		plugin.getConfig().set("last-memory-clean", Long.valueOf(System.currentTimeMillis()));
+	}
+
+	public void registerChunk(Block b) {
+		int x = (int) Math.floor(b.getX() / 128.0D) * 128;
+		int z = (int) Math.floor(b.getZ() / 128.0D) * 128;
+		Location loc = new Location(b.getWorld(), x, 0.0D, z);
+		if (!editedChunks.contains(loc)) {
+			editedChunks.add(loc);
+		}
+	}
+
+	private JSONArray getChunkCache() throws FileNotFoundException, IOException, ParseException {
+		JSONParser parser = new JSONParser();
+
+		Object file = parser.parse(new FileReader(plugin.getDataFolder() + "/chunkCache.json"));
+
+		return (JSONArray) file;
+	}
+
+	private void writeChunkCache(JSONArray data) throws IOException {
+		FileWriter cacheFile = new FileWriter(plugin.getDataFolder() + "/chunkCache.json");
+		cacheFile.write(data.toJSONString());
+		cacheFile.close();
+	}
+
+	private int[] generateMap(World w, int xCenter, int zCenter, boolean useAsCenter, int width, int height) {
+		int[] img = new int[16384];
+
+		HashMap<Chunk, Boolean> chunkStates = new HashMap<Chunk, Boolean>();
+
+		int minX = 0;
+		int minZ = 0;
+
+		if (useAsCenter) {
+			minX = xCenter - width / 2;
+			minZ = zCenter - height / 2;
+		} else {
+			minX = (int) (Math.floor(xCenter / width) * width);
+			minZ = (int) (Math.floor(zCenter / height) * height);
+		}
+
+		for (int z = 0; z < height; z++) {
+			for (int x = 0; x < width; x++) {
+				Block b = getHighestSolidAt(w, x + minX, z + minZ);
+				chunkStates.putIfAbsent(b.getChunk(), Boolean.valueOf(b.getChunk().isLoaded()));
+
+				try {
+					if(colorIndex.get(b.getType()) == 12) { //Water, color is depth dependant
+						if(getHighestSolidAt(w, x + minX, z + minZ - 1).getType() == Material.WATER) {
+							if(b.getY() - getLowestWaterBlock(w, x + minX, z + minZ).getY() > 3) { //Darker color (1st variant)
+								img[x + z * x] = colorIndex.get(b.getType()) * 4;
+							} else if(b.getY() - getLowestWaterBlock(w, x + minX, z + minZ).getY()  <= 3 && b.getY() - getLowestWaterBlock(w, x + minX, z + minZ).getY() > 1) { //Normal color (2nd variant)
+								img[x + z * x] = colorIndex.get(b.getType()) * 4 + 1;
+							} else if(b.getY() - getLowestWaterBlock(w, x + minX, z + minZ).getY() <= 1) { //Ligher color (3rd variant aka base color)
+								img[x + z * x] = colorIndex.get(b.getType()) * 4 + 2;
+							}		
+						} else {
+							img[x + z * x] = colorIndex.get(b.getType()) * 4;
+						}
+					} else { //Other blocks, color depends of the Y value of the block north of it.
+						if(getHighestSolidAt(w, x + minX, z + minZ - 1).getY() > b.getY()) { //Darker color (1st variant)
+							img[x + z * x] = colorIndex.get(b.getType()) * 4;
+						} else if(getHighestSolidAt(w, x + minX, z + minZ - 1).getY() == b.getY()) { //Normal color (2nd variant)
+							img[x + z * x] = colorIndex.get(b.getType()) * 4 + 1;
+						} else if(getHighestSolidAt(w, x + minX, z + minZ - 1).getY() < b.getY()) { //Ligher color (3rd variant aka base color)
+							img[x + z * x] = colorIndex.get(b.getType()) * 4 + 2;
+						}	
+					}
+				} catch(NullPointerException e) {
+					plugin.getLogger().warning("Unknown Material: " + b.getType() + "\n");
+					e.printStackTrace();
+				}		
+			}
+		}
+
+		for (Map.Entry<Chunk, Boolean> chunkState : chunkStates.entrySet()) {
+			if (!chunkState.getValue()) {
+				chunkState.getKey().unload();
+			}
+		}
+
+		return img;
+	}
+
+	private Block getHighestSolidAt(World w, int x, int z) {
+		int y = w.getHighestBlockYAt(x, z);
+		while(colorIndex.get(w.getBlockAt(x, y, z).getType()) == 0) { //Skip transparent blocks
+			y--;
+		}
+		
+		return w.getBlockAt(x, y, z);
+	}
+	
+	private Block getLowestWaterBlock(World w, int x, int z) {
+		int y = getHighestSolidAt(w, x, z).getY();
+		
+		while(colorIndex.get(w.getBlockAt(x, y, z).getType()) == 12) { //Water blocks
+			y--;
+		}
+		
+		return w.getBlockAt(x, y + 1, z);
+	}
+}
---
+++ src/tk/projectcraftmc/updater/UpdaterMain.java
@@ -0,0 +1,243 @@
+package tk.projectcraftmc.updater;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import javax.net.ssl.HttpsURLConnection;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+@SuppressWarnings("unchecked")
+public class UpdaterMain extends JavaPlugin implements Listener {
+
+	private String lastData;
+	
+	private Runnable runnable;
+	
+	private JSONArray left;
+	private JSONArray joined;
+	private JSONArray chat;
+	
+	private int rateLimitCheck;
+	private boolean rateLimited;
+	
+	private Mapper mapper;
+	
+	private boolean cancel;
+	private int currentTaskId;
+
+	@Override
+	public void onEnable() {
+		saveDefaultConfig();
+		getConfig().options().copyDefaults(true);
+
+		mapper = new Mapper(this);
+
+		rateLimitCheck = 0;
+		rateLimited = false;
+
+		getServer().getPluginManager().registerEvents(this, this);
+
+		left = new JSONArray();
+		joined = new JSONArray();
+		chat = new JSONArray();
+
+		lastData = "";
+
+		cancel = false;
+
+		runnable = new Runnable() {
+			public void run() {
+				JSONObject data = new JSONObject();
+				JSONArray players = new JSONArray();
+
+				JSONArray currentLeft = left;
+				JSONArray currentJoined = joined;
+				JSONArray currentChat = chat;
+
+				for (Player p : getServer().getOnlinePlayers()) {
+					JSONObject player = new JSONObject();
+					player.put("username", p.getPlayerListName());
+					player.put("uuid", p.getUniqueId().toString());
+					player.put("x", Integer.valueOf(p.getLocation().getBlockX()));
+					player.put("y", Integer.valueOf(p.getLocation().getBlockY()));
+					player.put("z", Integer.valueOf(p.getLocation().getBlockZ()));
+					player.put("dim", p.getWorld().getEnvironment().toString());
+					players.add(player);
+				}
+
+				data.put("players", players);
+				data.put("left", currentLeft);
+				data.put("joined", currentJoined);
+				if (getConfig().getBoolean("send-chat"))
+					data.put("chat", currentChat);
+
+				data.put("current-time", Long.valueOf(System.currentTimeMillis()));
+				data.put("refresh-time", Integer.valueOf(getConfig().getInt("delay-time")));
+
+				try {
+					if (!rateLimited && !data.toJSONString().equals(lastData)) {
+						sendDataToWebserver("data=" + data.toJSONString(), getConfig().getString("api-url"));
+						lastData = data.toJSONString();
+					} else {
+						if (getConfig().getBoolean("logging")) {
+							getLogger().info("Rate-limit mode active, not sending data. (Rate-limit counter: " + rateLimitCheck + ").");
+						}
+
+						rateLimitCheck = rateLimitCheck - 1;
+
+						if (rateLimitCheck == 0) {
+							rateLimited = false;
+						}
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				left.removeAll(currentLeft);
+				joined.removeAll(currentJoined);
+				chat.removeAll(currentChat);
+
+				if (System.currentTimeMillis() - getConfig().getLong("last-render-update") > (getConfig().getInt("render-update-time") * 1000)) {
+					mapper.updateMap();
+				}
+
+				if (System.currentTimeMillis() - getConfig().getLong("last-memory-clean") > (getConfig().getInt("memory-clean-update-time") * 1000)) {
+					mapper.saveEditedChunks();
+				}
+
+				if (cancel) {
+					getServer().getScheduler().cancelTask(currentTaskId);
+				}
+			}
+		};
+
+		if (!(new File(getDataFolder() + "/chunkCache.json")).exists()) {
+			saveResource("chunkCache.json", false);
+		}
+	}
+
+	public void onDisable() {
+		getServer().getScheduler().cancelTasks(this);
+	}
+
+	public void sendDataToWebserver(String data, String URL) throws IOException {
+		URL serverURL = new URL(URL);
+		HttpsURLConnection conn = (HttpsURLConnection) serverURL.openConnection();
+
+		conn.setRequestMethod("POST");
+
+		conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+		conn.setRequestProperty("Content-Type", "application/x-www-form-encoded; charset=utf-8");
+
+		DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+
+		out.write(data.getBytes(StandardCharsets.UTF_8));
+
+		out.flush();
+
+		out.close();
+
+		conn.disconnect();
+	}
+
+	public String getDataFromWebserver(String URL) throws IOException {
+		URL serverURL = new URL(URL);
+		HttpsURLConnection conn = (HttpsURLConnection) serverURL.openConnection();
+
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+		if (conn.getResponseCode() > 200) {
+			throw new ConnectException("Couldn't get data from the webserver. (" + conn.getResponseCode() + " " + conn.getResponseMessage() + ")");
+		}
+
+		String input = "";
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+		while ((input = String.valueOf(input) + in.readLine()) != null)
+			;
+
+		in.close();
+
+		return input;
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onJoin(PlayerJoinEvent e) {
+		JSONObject player = new JSONObject();
+		player.put("username", e.getPlayer().getPlayerListName());
+		player.put("uuid", e.getPlayer().getUniqueId().toString());
+		joined.add(player);
+
+		if (getServer().getOnlinePlayers().size() == 1) {
+			getServer().getScheduler().cancelTasks(this);
+
+			currentTaskId = getServer().getScheduler().scheduleSyncRepeatingTask(this, runnable, 100L, getConfig().getInt("delay-time") * 20L);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onLeave(PlayerQuitEvent e) {
+		JSONObject player = new JSONObject();
+		player.put("username", e.getPlayer().getPlayerListName());
+		player.put("uuid", e.getPlayer().getUniqueId().toString());
+		left.add(player);
+
+		if (getServer().getOnlinePlayers().size() == 1) {
+			cancel = true;
+		}
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onChat(AsyncPlayerChatEvent e) {
+		JSONObject chatMessage = new JSONObject();
+		chatMessage.put("sender", e.getPlayer().getPlayerListName());
+		chatMessage.put("time", Long.valueOf(System.currentTimeMillis()));
+		chatMessage.put("message", e.getMessage());
+		chat.add(chatMessage);
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onBlockBreak(BlockBreakEvent e) {
+		mapper.registerChunk(e.getBlock());
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onBlockPlace(BlockPlaceEvent e) {
+		mapper.registerChunk(e.getBlock());
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String commandlabel, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("genmap")) {
+			if (sender instanceof Player) {
+				Player p = (Player) sender;
+				mapper.registerChunk(p.getLocation().getBlock());
+				mapper.updateMap();
+			} else {
+				sender.sendMessage("player only");
+			}
+		}
+
+		return false;
+	}
+}
